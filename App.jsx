1ac1342df1f3e287bcd0e863a9831308ba70e344
swapnil agrawal <swapnilagrawal259@gmail.com>
2025-01-06 14:55:07 +0530

    init:project setup

---
+++ App.jsx
@@ -0,0 +1,30 @@
+
+
+import React from 'react';
+import {
+
+  Text,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = ()=>{
+  return (
+     <View
+          style={{flex:1,
+            backgroundColor: Colors.white,
+          }}>
+         <Text>
+            Welcome to React Native!
+         </Text>
+          
+        </View>
+  );
+}
+
+
+
+export default App;
